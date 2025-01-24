06944dcc98443ed520e1564923eb62ba435b5ee1
vlupato <vlupato72@gmail.com>
2025-01-24 00:35:25 -0300

    Cria funções: adicionar amigo na lista e alerta campo vazio

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+// Cria função para adicionar amigos
+
+function adicionarAmigo() {
+  let nomeDigitado = document.getElementById("amigo");
+  let nomeDoAmigo = nomeDigitado.value;
+  console.log(nomeDoAmigo);
+
+  // Cria alerta para campo vazio
+
+  if (nomeDoAmigo === "") {
+    alert("Por favor, insira um nome.");
+  }
+  // Adiciona o nome digitado na lista
+
+  amigos.push(nomeDoAmigo);
+  nomeDigitado.value = "";
+  atualizarListaAmigos();
+}
+
